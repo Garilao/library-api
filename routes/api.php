5253b6f42a0a52084c9635c989fc9c8cc764dbc7
Garilao <jaelamaestaanagarilao@gmail.com>
2025-12-10 18:20:14 +0800

    add swagger and fix documentation

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,30 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\BorrowController;
+use Illuminate\Http\Request;
 
-Route::post('/register', [AuthController::class,'register']);
-Route::post('/login', [AuthController::class,'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class,'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::get('/user', function(Request $r){ return $r->user(); });
+    Route::get('/user', function (Request $r) {
+        return $r->user();
+    });
 
-    Route::get('/books', [BookController::class,'index']);
-    Route::get('/books/{book}', [BookController::class,'show']);
-    Route::post('/books', [BookController::class,'store']);
-    Route::put('/books/{book}', [BookController::class,'update']);
-    Route::delete('/books/{book}', [BookController::class,'destroy']);
+    // Book routes
+    Route::get('/books', [BookController::class, 'index']);
+    Route::get('/books/{book}', [BookController::class, 'show']);
+    Route::post('/books', [BookController::class, 'store']);
+    Route::put('/books/{book}', [BookController::class, 'update']);
+    Route::delete('/books/{book}', [BookController::class, 'destroy']);
 
-    Route::post('/borrow', [BorrowController::class,'borrow']);
-    Route::post('/return', [BorrowController::class,'return']);
-    Route::get('/borrow/history', [BorrowController::class,'history']);
+    // Borrowing routes
+    Route::post('/borrow', [BorrowController::class, 'borrow']);
+    Route::post('/return', [BorrowController::class, 'return']);
+    Route::get('/borrow/history', [BorrowController::class, 'history']);
 });
